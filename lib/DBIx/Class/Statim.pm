641be3284045bf7294f70f5a82a048eb926bd2af
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-09-04 21:18:35 -0300

    Add pod documentation.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

--- lib/DBIx/Class/Statim.pm
+++ lib/DBIx/Class/Statim.pm
@@ -7,13 +7,73 @@ use warnings;
 use base qw( DBIx::Class );
 use Statim::Client;
 
-our $VERSION = '0.0001';
+# ABSTRACT: Convenient way to use Statim with DBIx::Class.
+# VERSION
+
+=head1 NAME
+
+DBIx::Class::Statim - Convenient way to use Statim with DBIx::Class.
+
+=head1 SYNOPSIS
+
+   package StatimTest::Schema::Log;
+
+   use strict;
+   use warnings;
+
+   use base qw( DBIx::Class );
+
+   __PACKAGE__->load_components(qw(PK::Auto Core ));
+   __PACKAGE__->table('log');
+
+    __PACKAGE__->add_columns(
+        id => { is_auto_increment => 1 },
+        qw/customer_id status/
+    );
+
+    __PACKAGE__->set_primary_key( 'id' );
+
+    __PACKAGE__->statim_host('127.0.0.1');
+    __PACKAGE__->statim_port('54130');
+    __PACKAGE__->statim_collection('log');
+    __PACKAGE__->statim_enum_cols('customer_id', 'status' );
+    __PACKAGE__->statim_count_col('entry');
+
+    1;
+
+
+=head1 GETTING HELP/SUPPORT
+
+The community can be found via:
+
+=over
+
+=item * IRC: irc.perl.org#sao-paulo.pm
+
+=item * Github: L<http://github.com/maluco/DBIx-Class-Statim>
+
+=back
+
+=cut
 
 __PACKAGE__->mk_classdata( 'statim_host' => '127.0.0.1' );
 __PACKAGE__->mk_classdata( 'statim_port' => 0 );
 __PACKAGE__->mk_classdata( 'statim_collection' => 'collection' );
+
+sub new {
+    my $class = shift;
+    my $data = shift;
+
+    my @args;
+    map {
+        push( @args, join(':', $_, $data->{$_}) );
+    } grep { ! /-result_source/ } keys %$data;
+    my $ret = $class->next::method($data, @_);
+    $class->update_statim(@args);
+    return $ret;
+}
+
 __PACKAGE__->mk_classdata( '_enum_cols'  => () );
-__PACKAGE__->mk_classdata( '_count_col'  => '' );
 
 sub statim_enum_cols {
     my $class = shift;
@@ -23,6 +83,8 @@ sub statim_enum_cols {
     return $class->_enum_cols;
 }
 
+__PACKAGE__->mk_classdata( '_count_col'  => '' );
+
 sub statim_count_col {
     my $class = shift;
     if (@_) {
@@ -30,19 +92,6 @@ sub statim_count_col {
         $class->_count_col($col);
     }
     return $class->_count_col;
-}
-
-sub new {
-    my $class = shift;
-    my $data = shift;
-    
-    my @args;
-    map { 
-        push( @args, join(':', $_, $data->{$_}) );
-    } grep { ! /-result_source/ } keys %$data;
-    my $ret = $class->next::method($data, @_);
-    $class->update_statim(@args);
-    return $ret;
 }
 
 sub update_statim {
@@ -60,6 +109,4 @@ sub update_statim {
 
 }
 
-
 1;
-
